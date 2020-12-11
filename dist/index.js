7752d25dc80424bafa51a20a559f7be9a2ca0372
Mico <mico@cynder.io>
2020-12-11 20:15:57 +0800

    chore: rebuilt dist package

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -35,6 +46,17 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -53,16 +75,16 @@ var Paymongo = /** @class */ (function () {
             if (method === void 0) { method = 'GET'; }
             return superagent_1.default(method, "" + _this.baseUrl + url);
         };
-        this.createPaymentIntent = function (amount) { return __awaiter(_this, void 0, void 0, function () {
-            var payload, result;
+        this.createPaymentIntent = function (attributes) { return __awaiter(_this, void 0, void 0, function () {
+            var amount, rest, payload, result;
             return __generator(this, function (_a) {
                 switch (_a.label) {
                     case 0:
-                        payload = this.constructPayload({
-                            amount: amount * 100,
-                            payment_method_allowed: ['card'],
-                            currency: 'PHP',
-                        });
+                        amount = attributes.amount, rest = __rest(attributes, ["amount"]);
+                        console.log('Woah', amount);
+                        payload = this.constructPayload(__assign(__assign({}, rest), { amount: amount * 100, 
+                            /** Override values for now until API updates */
+                            payment_method_allowed: ['card'], currency: 'PHP' }));
                         return [4 /*yield*/, this.sendRequest('/payment_intents', 'POST').set(this.getHeaders()).send(payload)];
                     case 1:
                         result = _a.sent();
---
+++ dist/interfaces/paymentIntent.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ThreeDeeSecureModes;
+(function (ThreeDeeSecureModes) {
+    ThreeDeeSecureModes["Any"] = "any";
+    ThreeDeeSecureModes["Automatic"] = "automatic";
+})(ThreeDeeSecureModes || (ThreeDeeSecureModes = {}));
+var PaymentMethodAllowed;
+(function (PaymentMethodAllowed) {
+    PaymentMethodAllowed["Cash"] = "cash";
+})(PaymentMethodAllowed || (PaymentMethodAllowed = {}));
